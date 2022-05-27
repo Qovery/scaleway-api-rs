131c2aee6e0a0232128979fe93dd2e241cc3beb4
Gaëtan <gaetan-github@pignouf.fr>
2022-05-27 11:04:26 +0200

    Update Openapi urls

--- generate_api.sh
+++ generate_api.sh
@@ -12,14 +12,14 @@ mkdir .generation
 
 # add open spec API file here
 # =>
-wget -P .generation/ https://developers.scaleway.com/static/53eca7755870a063a29d172455c9a5a3/scaleway.baremetal.v1.Api.yml
-wget -P .generation/ https://developers.scaleway.com/static/991ccc31ec600bedad34d7e8382799ec/scaleway.rdb.v1.Api.yml
-wget -P .generation/ https://developers.scaleway.com/static/7f404aaae3cabd6b26f7e5f2a1e32e05/scaleway.registry.v1.Api.yml
-wget -P .generation/ https://developers.scaleway.com/static/be99ddac31d6fc16d3797937da043821/scaleway.k8s.v1.Api.yml
-wget -P .generation/ https://developers.scaleway.com/static/78dc39f6f39f212eff8813334e33fc65/scaleway.vpc.v1.Api.yml
-wget -P .generation/ https://developers.scaleway.com/static/72a2c7b853875ff3d1470f3453d8cd9d/scaleway.iot.v1.Api.yml
-wget -P .generation/ https://developers.scaleway.com/static/124134235ef10c3bf8c87f813ab7a589/scaleway.flexible_ip.v1alpha1.Api.yml
-wget -P .generation/ https://developers.scaleway.com/static/89ef890736cb4e3d26984aaaf9c9b1e8/scaleway.domain.v2beta1.Api.yml
+wget -P .generation/ https://developers.scaleway.com/static/c78c1fb6947abf78efab65d4acc6dacd/scaleway.baremetal.v1.Api.yml
+wget -P .generation/ https://developers.scaleway.com/static/805c4e0fa44967438ff07c28d87fac04/scaleway.rdb.v1.Api.yml 
+wget -P .generation/ https://developers.scaleway.com/static/7f404aaae3cabd6b26f7e5f2a1e32e05/scaleway.registry.v1.Api.yml 
+wget -P .generation/ https://developers.scaleway.com/static/72af768aeae6f8e82ef2ea61b4e46975/scaleway.k8s.v1.Api.yml 
+wget -P .generation/ https://developers.scaleway.com/static/c6dfc13b6013041388d93a1f54f77abb/scaleway.vpc.v1.Api.yml 
+wget -P .generation/ https://developers.scaleway.com/static/72a2c7b853875ff3d1470f3453d8cd9d/scaleway.iot.v1.Api.yml 
+wget -P .generation/ https://developers.scaleway.com/static/0bc750daa510b57996c29662eb213213/scaleway.flexible_ip.v1alpha1.Api.yml 
+wget -P .generation/ https://developers.scaleway.com/static/e68e360c068c64e2fabb3de44f988646/scaleway.domain.v2beta1.Api.yml 
 
 # merging specs into one file
 cat << EOM > .generation/openapi-merge.json
